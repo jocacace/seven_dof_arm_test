e2a1bd7d010cee73598f3c35da6245ac04b93e44
Jonathan Cacace <jonathan.cacace@gmail.com>
2017-12-07 11:21:45 +0100

    added model files

--- src/remove_collision_objct.cpp
+++ src/remove_collision_objct.cpp
@@ -1,4 +1,4 @@
-#include <moveit/move_group_interface/move_group.h>
+#include <moveit/move_group_interface/move_group_interface.h>
 #include <moveit/planning_scene_interface/planning_scene_interface.h>
 
 int main(int argc, char **argv)
